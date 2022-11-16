2f724e06e705809b783259920e3d36910c0fd06e
Tristan Timblin <ttimblin@yext.com>
2022-11-16 11:49:14 -0500

    revert story style

--- tests/components/Address.stories.tsx
+++ tests/components/Address.stories.tsx
@@ -1,4 +1,4 @@
-import { ComponentMeta } from "@storybook/react";
+import { ComponentMeta, Story } from "@storybook/react";
 import { Address, AddressProps } from "../../src/components/Address/Address";
 import { US_ADDRESS, BR_ADDRESS } from "../__fixtures__/km/address";
 
@@ -9,10 +9,18 @@ const meta: ComponentMeta<typeof Address> = {
 
 export default meta;
 
-export const Primary = (args: Partial<AddressProps>) => {
-  return <Address address={US_ADDRESS} {...args} />;
+export const Primary: Story<AddressProps> = (args) => {
+  return <Address {...args} />;
 };
 
-export const Brazil = (args: Partial<AddressProps>) => {
-  return <Address address={BR_ADDRESS} {...args} />;
+Primary.args = {
+  address: US_ADDRESS,
+};
+
+export const Brazil: Story<AddressProps> = (args) => {
+  return <Address {...args} />;
+};
+
+Brazil.args = {
+  address: BR_ADDRESS,
 };
