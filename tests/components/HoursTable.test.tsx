d03eda361ccd4f028dcc948a782c58606310df5f
Tom Meyer <tmeyer@yext.com>
2023-03-07 10:30:08 -0500

    Fix tests.

--- tests/components/HoursTable.test.tsx
+++ tests/components/HoursTable.test.tsx
@@ -138,9 +138,8 @@ describe("format", () => {
 describe("parseTime", () => {
   it("transforms Yext Time string to a JS localized time string", () => {
     const actual = parseTime("09:00");
-    const expected = "9:00 AM";
-
-    expect(actual).toBe(expected);
+    
+    expect(actual).toMatch(/^9:00[\s\p{u+202f}]AM$/);
   });
 
   it("returns a custom JS localized time string", () => {
