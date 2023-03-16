6303b35c84088348be8f8d604f62e1b56f5b14c6
Tom Meyer <tmeyer@yext.com>
2023-03-16 13:34:56 -0400

    Styles.

--- src/components/richText/DefaultStyling.ts
+++ src/components/richText/DefaultStyling.ts
@@ -1,6 +1,20 @@
 import { EditorThemeClasses } from "lexical";
 
 /**
+ * Tailwind styling for the different Tokens that can occur within a Code Block.
+ * Token Types come from the PrismJS library, which Lexical uses to power Code Blocks.
+ * The Types include Comment, Variable, Operator, etc.
+ */
+const attributeTokenStyles = "text-sky-700";
+const propertyTokenStyles = "text-pink-800";
+const commentTokenStyles = "text-slate-500";
+const functionTokenStyles = "text-rose-500";
+const selectorTokenStyles = "text-lime-600";
+const variableTokenStyles = "text-amber-500";
+const operatorTokenStyles = "text-yellow-700";
+const punctuationTokenStyles = "text-neutral-400";
+
+/**
  * Default styling to apply to the different types of Lexical Rich Text
  * Elements.
  */
@@ -22,6 +36,38 @@ const DefaultNodeStyling: EditorThemeClasses = {
     ul: "p-0 m-0 ml-4 list-disc",
     ol: "p-0 m-0 ml-4, list-decimal",
     listitem: "mx-8 my-2",
+  },
+  code: "bg-gray-100 block text-xs m-0 mt-2 mb-2 overflow-x-auto relative p-2",
+  codeHighlight: {
+    atrule: attributeTokenStyles,
+    attr: attributeTokenStyles,
+    boolean: propertyTokenStyles,
+    builtin: selectorTokenStyles,
+    cdata: commentTokenStyles,
+    char: selectorTokenStyles,
+    class: functionTokenStyles,
+    comment: commentTokenStyles,
+    constant: propertyTokenStyles,
+    deleted: propertyTokenStyles,
+    doctype: commentTokenStyles,
+    entity: operatorTokenStyles,
+    function: functionTokenStyles,
+    important: variableTokenStyles,
+    inserted: selectorTokenStyles,
+    keyword: attributeTokenStyles,
+    namespace: variableTokenStyles,
+    number: propertyTokenStyles,
+    operator: operatorTokenStyles,
+    prolog: commentTokenStyles,
+    property: propertyTokenStyles,
+    punctuation: punctuationTokenStyles,
+    regex: variableTokenStyles,
+    selector: selectorTokenStyles,
+    string: selectorTokenStyles,
+    symbol: propertyTokenStyles,
+    tag: propertyTokenStyles,
+    url: operatorTokenStyles,
+    variable: variableTokenStyles
   },
   quote: "m-0 ml-5 text-sm text-gray-500 border-l-4 pl-4",
   table: "w-11/12 max-w-full overflow-y-scroll my-7",
