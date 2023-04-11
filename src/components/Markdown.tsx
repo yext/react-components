d4b08b4da1aaf5bf4e96cb5115f43e64ec6f7934
Tom Meyer <tmeyer@yext.com>
2023-04-11 11:56:02 -0400

    Support a CommonJS build of the `Markdown` Component. (#23)
    Previously, the `Markdown` Component did not work with CommonJS. This
    is because the versions of `react-markdown`, `remark-gfm`, etc. were
    ESM-only. In order to have a CommonJS-compatible Component, I needed to
    downgrade these and other libraries used by `Markdown`.
    
    Initially, the decision was made intentionally to only have an ESM-compatible
    `Markdown` Component. However, that's proven troublesome for PagesJS users.
    Now that there is a CommonJS variant of `Markdown`, I was able to add Jest
    coverage. The Jest tests were essentially ported from the tests KG had in Alpha.
    
    The older version of `rehype-sanitize` does not support user-supplied Schema.
    That's okay, however. Product and I decided this was not an important feature
    after all.
    
    TEST=auto,manual
    
    Ran all unit tests. Manually ensured that HTML was getting sanitized properly.

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -1,46 +1,29 @@
-import ReactMarkdown from "react-markdown";
+import ReactMarkdown, { PluggableList } from "react-markdown";
 import remarkGfm from "remark-gfm";
 import rehypeRaw from "rehype-raw";
-import rehypeSanitize, { Options } from "rehype-sanitize";
-import { useMemo } from "react";
-import { PluggableList } from "unified";
+import rehypeSanitize from "rehype-sanitize";
+
+// The Remark and Rehype plugins to use in conjunction with ReactMarkdown.
+const unifiedPlugins: { remark: PluggableList; rehype: PluggableList } = {
+  remark: [remarkGfm],
+  rehype: [rehypeRaw, rehypeSanitize],
+};
 
 export interface MarkdownProps {
   /** Stringified Github-Flavored Markdown. */
   content: string;
-  /** HTML Sanitization Schema for use with Rehype. */
-  sanitizationSchema?: Options;
 }
 
 /**
  * Renders Github-Flavored Markdown from the Knowledge Graph. This Markdown can include
- * arbitrary HTML. Any HTML will be sanitized according to the provided Rehype Schema. If
- * no Schema is provided, Rehype's default will be used.
+ * arbitrary HTML. Any HTML will be sanitized according to Rehype's default Schema.
  */
-export function Markdown({ content, sanitizationSchema }: MarkdownProps) {
-  const plugins = useMemo(() => {
-    const unifiedPlugins: { remark: PluggableList; rehype: PluggableList } = {
-      remark: [remarkGfm],
-      rehype: [rehypeRaw],
-    };
-
-    if (sanitizationSchema) {
-      unifiedPlugins.rehype.push([
-        rehypeSanitize,
-        { options: sanitizationSchema },
-      ]);
-    } else {
-      unifiedPlugins.rehype.push(rehypeSanitize);
-    }
-
-    return unifiedPlugins;
-  }, [sanitizationSchema]);
-
+export function Markdown({ content }: MarkdownProps) {
   return (
     <ReactMarkdown
       children={content}
-      remarkPlugins={plugins.remark}
-      rehypePlugins={plugins.rehype}
+      remarkPlugins={unifiedPlugins.remark}
+      rehypePlugins={unifiedPlugins.rehype}
     />
   );
 }
