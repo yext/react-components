b24d3b79164846f85a9f92dc9d3e68b66b224a78
Tom Meyer <tmeyer@yext.com>
2023-04-11 10:38:21 -0400

    Support a CommonJS build of the `Markdown` Component.
    Previously, the `Markdown` Component did not work with CommonJS. This
    is because the versions of `react-markdown`, `remark-gfm`, etc. were
    ESM-only. In order to have a CommonJS-compatible Component, I needed to
    downgrade these and other libraries used by `Markdown`.
    
    Initially, the decision was made intentionally to only have an ESM-compatible
    `Markdown` Component. However, that's proven troublesome for PagesJS users.
    Now that there is a CommonJS variant of `Markdown`, I was able to add Jest
    coverage. The Jest tests were essentially ported from the tests KG had in Alpha.
    
    The older version of `rehype-sanitize` does not support user-supplied Schema.
    That's okay, however. Product and I decided this was not an important feature
    after all.
    
    TEST=auto,manual
    
    Ran all unit tests. Manually ensured that HTML was getting sanitized properly.

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -1,40 +1,26 @@
-import ReactMarkdown from "react-markdown";
+import ReactMarkdown, { PluggableList } from "react-markdown";
 import remarkGfm from "remark-gfm";
 import rehypeRaw from "rehype-raw";
-import rehypeSanitize, { Options } from "rehype-sanitize";
+import rehypeSanitize from "rehype-sanitize";
 import { useMemo } from "react";
-import { PluggableList } from "unified";
-
 export interface MarkdownProps {
   /** Stringified Github-Flavored Markdown. */
   content: string;
-  /** HTML Sanitization Schema for use with Rehype. */
-  sanitizationSchema?: Options;
 }
 
 /**
  * Renders Github-Flavored Markdown from the Knowledge Graph. This Markdown can include
- * arbitrary HTML. Any HTML will be sanitized according to the provided Rehype Schema. If
- * no Schema is provided, Rehype's default will be used.
+ * arbitrary HTML. Any HTML will be sanitized according to Rehype's default Schema.
  */
-export function Markdown({ content, sanitizationSchema }: MarkdownProps) {
+export function Markdown({ content }: MarkdownProps) {
   const plugins = useMemo(() => {
     const unifiedPlugins: { remark: PluggableList; rehype: PluggableList } = {
       remark: [remarkGfm],
-      rehype: [rehypeRaw],
+      rehype: [rehypeRaw, rehypeSanitize],
     };
 
-    if (sanitizationSchema) {
-      unifiedPlugins.rehype.push([
-        rehypeSanitize,
-        { options: sanitizationSchema },
-      ]);
-    } else {
-      unifiedPlugins.rehype.push(rehypeSanitize);
-    }
-
     return unifiedPlugins;
-  }, [sanitizationSchema]);
+  }, []);
 
   return (
     <ReactMarkdown
