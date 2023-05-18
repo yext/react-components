d3f9c5c68bcb24069d8ee81c64bb096df43e66c9
Tom Meyer <tmeyer@yext.com>
2023-05-18 11:02:44 -0400

    Restructure `index.ts` and add `"sideEffects": false` to improve tree-shaking.
    This PR makes a couple of changes to improve the tree-shakability of this
    library. Firstly, we no longer glob everything from `components/index.ts` in
    `index.ts`. This globbing caused the entire library to be loaded even if you
    were using just one Component in your application.
    
    The second change is adding `"sideEffects: false` to the `package.json`. You
    can read more about that attribute here: https://webpack.js.org/guides/tree-shaking/.
    Basically, this tells Rollup and Webpack that our library can be tree-shaken.
    
    TEST=manual
    
    Created a React application in both PagesJS and CRA that used our Component library.
    Saw that the bundle size for a page using one Component went from 500 kB to 150 kB.
    The bundle was tested to make sure the page rendered properly.

--- src/components/index.ts
+++
@@ -1,8 +0,0 @@
-export * from "./address";
-export * from "./image";
-export { Markdown, MarkdownProps } from "./Markdown";
-export { PinComponent, MapboxMapProps, MapboxMap } from "./MapboxMap";
-export {
-  LexicalRichText,
-  LexicalRichTextProps,
-} from "./richText/LexicalRichText";
