ba8a3fb211a4bdea33e925db71a083a7520ab737
Tom Meyer <tmeyer@yext.com>
2023-05-18 11:14:55 -0400

    Fix linting errors.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 export { Address, AddressProps } from "./components/address";
 export { Image, ImageProps } from "./components/image";
 export { Markdown, MarkdownProps } from "./components/Markdown";
-export { PinComponent, MapboxMapProps, MapboxMap } from "./components/MapboxMap";
+export {
+  PinComponent,
+  MapboxMapProps,
+  MapboxMap,
+} from "./components/MapboxMap";
 export {
   LexicalRichText,
   LexicalRichTextProps,
